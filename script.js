33ced2c2ebabeacaee912855ff043d63259014b5
Ula <ulaa93@gmail.com>
2020-02-29 19:40:29 +0100

    fixed sort

--- script.js
+++ script.js
@@ -1,75 +1,79 @@
 const app = document.querySelector('.appnew');
-const sortId = document.querySelector('.sort-id');
-const sortName = document.querySelector('.sort-name');
-const sortCity = document.querySelector('.sort-city');
-const sortTotal = document.querySelector('.sort-total');
-const sortAverage = document.querySelector('.sort-average');
-const sortLast = document.querySelector('.sort-last');
-const searchInput = document.getElementById('search');
+const paginationEl = document.querySelector('#pagination');
+const searchInput = document.querySelector('#search');
+let menuElements = document.getElementsByTagName('th');
 
 const companiesUrl = 'https://recruitment.hal.skygate.io/companies';
 const incomesUrl = 'https://recruitment.hal.skygate.io/incomes/';
 
 let companies = [];
+let lastMonth = [];
 
+//Get incomes for currency company
 const getIncome = param => {
   return fetch(`${incomesUrl}${param.id}`)
     .then(response => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      }
     })
     .then(res => {
-      // console.log(xd);
-      console.log(companies);
       let totalIncome = res.incomes.reduce((a, b) => {
         return a + parseFloat(b.value);
       }, 0);
       let averageIncome = totalIncome / res.incomes.length;
-      let el = companies.findIndex(el => el.id == param.id);
-      // console.log(el);
-      companies[el].totalIncomes = totalIncome.toFixed(2);
-      companies[el].average = averageIncome.toFixed(2);
-      // console.log(el);
-      // console.log(companies);
-      // render();
-      return companies;
+      let elem = companies.findIndex(el => el.id == param.id);
+      let latestData = new Date(
+        Math.max.apply(
+          null,
+          res.incomes.map(el => {
+            return new Date(el.date);
+          })
+        )
+      );
+      lastMonth = [];
+      res.incomes.map(el => {
+        if (
+          new Date(el.date).getMonth() === latestData.getMonth() &&
+          new Date(el.date).getFullYear() == latestData.getFullYear()
+        ) {
+          lastMonth.push(el);
+        }
+      });
+      let lastMonthValue = lastMonth.reduce((a, b) => {
+        return a + parseFloat(b.value);
+      }, 0);
+      companies[elem].totalIncomes = totalIncome.toFixed(2);
+      companies[elem].average = averageIncome.toFixed(2);
+      companies[elem].lastMonth = lastMonthValue.toFixed(2);
     })
-    .then(() => {
-      return companies;
-    });
-  // console.log(companies.id);
-  return companies;
-
-  //   console.log(xd);
-
-  //   return;
+    .catch(error => console.log(error));
 };
 
+//Get all info about companies
 const allData = () => {
   return fetch(companiesUrl)
     .then(response => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      }
     })
     .then(res => {
       companies = [...res];
       return companies;
     })
-    .then(() => {
-      return companies;
-    })
     .then(res => {
       for (let company of res) {
         getIncome(company).then(() => {
-          //   console.log(companies);
-          //   companies = companies;
-          //   return response;
           displayData(companies, app, rows, currentPage);
           setupPagination(companies, paginationEl, rows);
         });
       }
-    });
+    })
+    .catch(error => console.log(error));
 };
 
-const paginationEl = document.querySelector('#pagination');
+// Display data with pagination
 let currentPage = 1;
 let rows = 50;
 function displayData(items, wrapper, rowsPage, page) {
@@ -78,30 +82,25 @@ function displayData(items, wrapper, rowsPage, page) {
   let start = rowsPage * page;
   let end = start + rowsPage;
   let paginatedItems = items.slice(start, end);
-  //   console.log(paginatedItems);
   for (let i = 0; i < paginatedItems.length; i++) {
-    // console.log(items[i]);
     let item = paginatedItems[i];
     const tr = document.createElement('tr');
-    // console.log(companies);
-    // compare(res, id);
     let output = `
                 <td>${item.id}</td>
                 <td>${item.name}</td>
                 <td>${item.city}</td>
                 <td>${item.totalIncomes}</td>
                 <td>${item.average}</td>
-
-                <td></td>
+                <td>${item.lastMonth}</td>
              `;
-
     tr.innerHTML = output;
     wrapper.appendChild(tr);
   }
 }
+
+//Pagintation
 function setupPagination(items, wrapper, rowsPage) {
   wrapper.innerHTML = '';
-
   let pageCount = Math.ceil(items.length / rowsPage);
   for (let i = 1; i < pageCount + 1; i++) {
     let btn = paginationButton(i, items);
@@ -109,271 +108,41 @@ function setupPagination(items, wrapper, rowsPage) {
   }
 }
 
+//Pagination buttons
 function paginationButton(page, companies) {
   let button = document.createElement('button');
   button.innerText = page;
-
   if (currentPage == page) button.classList.add('active');
-
   button.addEventListener('click', function() {
     currentPage = page;
     displayData(companies, app, rows, currentPage);
-    // console.log(currentPage, page);
     let currentBtn = document.querySelector('button.active');
-    // console.log(currentBtn);
     currentBtn.classList.remove('active');
-
     button.classList.add('active');
   });
-
   return button;
 }
-//
-//
-//
-//END OF PAGINATION
-//
-//
-//
-//
-// DisplayList(list_items, list_element, rows, current_page);
-// SetupPagination(list_items, pagination_element, rows);
-//
-//
-//
-//
-//
-//
-//
-// app.addEventListener('load', () => {
-//   allData()
-//     .then(() => {
-//       return companies;
-//     })
-//     .then(() => {
-//       displayData(companies, app, rows, currentPage);
-//       setupPagination(companies, paginationEl, rows);
-//     });
-// });
-//
-//
-//
 
-// allData()
-//   .then(() => {
-//     return companies;
-//   })
-//   .then(() => {
-//     displayData(companies, app, rows, currentPage);
-//     setupPagination(companies, paginationEl, rows);
-//   });
-
-//
-//
-// allData();
-document.addEventListener(
-  'load',
-  allData()
-  // // .then(() => {
-  // //   companies = companies;
-  // //   return companies;
-  // // })
-  // .then(() => {
-  //   displayData(companies, app, rows, currentPage);
-  //   setupPagination(companies, paginationEl, rows);
-  // })
-);
-//
-//
-//
-
-// function sortData(param) {
-//     companies.sort((a, b) => {
-//       return a.param - b.param;
-//     });
-//     console.log(companies);
-//   }
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-//
-//
-//
-//
-let orderId = false;
-let orderName = false;
-let orderCity = false;
-let orderTotal = false;
-let orderAverage = false;
-let orderLast = false;
-
-// function sortData(x, y) {
-//   order = !order;
-//   if (typeof x === Number)
-//     companies = companies.sort((a, b) => {
-//       if (order) {
-//         return x - y;
-//       } else {
-//         return y - x;
-//       }
-//     });
-//   else {
-//     // order = !order;
-//     companies = companies.sort((a, b) => {
-//       if (order) {
-//         return x.localeCompare(y);
-//       } else {
-//         return y.localeCompare(x);
-//       }
-//     });
-//   }
-// }
-
-let order = false;
-function sortFun(param, eventAdd) {
-  //   var sortOrder = 1;
-  //   if (param[0] === '-') {
-  //     sortOrder = -1;
-  //     param = param.substr(1);
-  //   }
-  order = !order;
-  //   eventAdd.addEventListener('click', () => {
-  //     order = !order;
-  //     sortFun(param);
-  //   });
-  return function(a, b) {
-    if (order) {
-      return a[param] - b[param];
-    } else {
-      return b[param] - a[param];
+//Sort
+function sortFun(param, order) {
+  companies = companies.sort((a, b) => {
+    if (typeof a[param] === 'number') {
+      if (order) {
+        return a[param] - b[param];
+      } else {
+        return b[param] - a[param];
+      }
+    } else if (typeof a[param] === 'string') {
+      if (order) {
+        return a[param].localeCompare(b[param]);
+      } else {
+        return b[param].localeCompare(a[param]);
+      }
     }
-  };
+  });
 }
 
-function sortDyn(key, order = 'asc') {
-  //   if (order === 'desc') {
-  //     sort_order = -1;
-  //   }
-  return function(a, b) {
-    var sort_order = 0;
-
-    // a should come before b in the sorted order
-    if (a[key] < b[key]) {
-      return (sort_order = -1);
-      // a should come after b in the sorted order
-    } else if (a[key] > b[key]) {
-      return (sort_order = 1);
-      // a and b are the same
-    }
-    return order === 'desc' ? sort_order * -1 : sort_order;
-  };
-}
-
-// console.log(companies);
-sortId.addEventListener('click', () => {
-  // {
-
-  //   orderId = !orderId;
-  //   companies = companies.sort((a, b) => {
-  //     if (orderId) {
-  //       return a.id - b.id;
-  //     } else {
-  //       return b.id - a.id;
-  //     }
-  //   sortFun('id');
-  companies.sort(sortFun('id'));
-  //   companies.sort(sortDyn('id'));
-  // });
-
-  //   sortData(a.id, b.id);
-  displayData(companies, app, rows, currentPage);
-  setupPagination(companies, paginationEl, rows);
-});
-
-sortName.addEventListener('click', () => {
-  orderName = !orderName;
-  companies = companies.sort((a, b) => {
-    if (orderName) {
-      return a.name.localeCompare(b.name);
-    } else {
-      return b.name.localeCompare(a.name);
-    }
-  });
-  displayData(companies, app, rows, currentPage);
-  setupPagination(companies, paginationEl, rows);
-});
-
-sortCity.addEventListener('click', () => {
-  orderCity = !orderCity;
-  companies = companies.sort((a, b) => {
-    if (orderCity) {
-      return a.city.localeCompare(b.city);
-    } else {
-      return b.city.localeCompare(a.city);
-    }
-  });
-  displayData(companies, app, rows, currentPage);
-  setupPagination(companies, paginationEl, rows);
-});
-
-sortTotal.addEventListener('click', () => {
-  //   orderTotal = !orderTotal;
-  //   companies = companies.sort((a, b) => {
-  //     if (orderTotal) {
-  //       return a.totalIncomes - b.totalIncomes;
-  //     } else {
-  //       return b.totalIncomes - a.totalIncomes;
-  //     }
-  //   });
-  companies.sort(sortFun('totalIncomes'));
-
-  displayData(companies, app, rows, currentPage);
-  setupPagination(companies, paginationEl, rows);
-});
-
-sortAverage.addEventListener('click', () => {
-  orderAverage = !order;
-  companies = companies.sort((a, b) => {
-    if (order) {
-      return a.average - b.average;
-    } else {
-      return b.average - a.average;
-    }
-  });
-  displayData(companies, app, rows, currentPage);
-  setupPagination(companies, paginationEl, rows);
-});
-// sortLast.addEventListener(
-//   'click',
-//   allData().then(companies => {
-//     companies = companies.sort((a, b) => {
-//       return a.lastIncome - b.lastIncome;
-//     });
-//     displayData(companies, app, rows, currentPage);
-//     setupPagination(companies, paginationEl, rows);
-//   })
-// );
-
-//
-let result = [];
-searchInput.addEventListener('keyup', () => {
-  searchData();
-  displayData(result, app, rows, currentPage);
-  setupPagination(result, paginationEl, rows);
-});
-
+//Search
 function searchData() {
   filterData = searchInput.value.toUpperCase();
   result = [];
@@ -390,4 +159,25 @@ function searchData() {
       }
     }
   }
+}
+
+// Added event listeners
+document.addEventListener('load', allData());
+
+searchInput.addEventListener('keyup', () => {
+  searchData();
+  displayData(result, app, rows, currentPage);
+  setupPagination(result, paginationEl, rows);
+});
+
+for (let c = 0; c < menuElements.length; c++) {
+  let order = false;
+  menuElements[c].addEventListener('click', () => {
+    let keys = Object.keys(companies[0]);
+    let accurateKey = keys[c];
+    order = !order;
+    sortFun(accurateKey, order);
+    displayData(companies, app, rows, currentPage);
+    setupPagination(companies, paginationEl, rows);
+  });
 }
